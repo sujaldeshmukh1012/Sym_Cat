34eeb0d7dbbc8cee4928c57164f763e2cf14f8af
Sujal Bhakare <sujalbhakare@eduroamprvnat-172-16-60-229.near.illinois.edu>
2026-02-27 22:48:00 -0600

    XCODE ADDED, Design rules added

---
+++ CAT Inspect/CAT Inspect/CAT_InspectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CAT_InspectApp.swift
+//  CAT Inspect
+//
+//  Created by Sujal Bhakare on 2/27/26.
+//
+
+import SwiftUI
+
+@main
+struct CAT_InspectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CAT Inspect/CAT InspectTests/CAT_InspectTests.swift
@@ -0,0 +1,17 @@
+//
+//  CAT_InspectTests.swift
+//  CAT InspectTests
+//
+//  Created by Sujal Bhakare on 2/27/26.
+//
+
+import Testing
+@testable import CAT_Inspect
+
+struct CAT_InspectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
